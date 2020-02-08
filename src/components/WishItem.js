98e3279777586dee8124731b6c93701475b0db1c
cs-daviddavid <cs@davidcyrildavid.com>
2020-02-08 15:47:28 -0500

    WishItem, 50% Complete

--- src/components/WishItem.js
+++ src/components/WishItem.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 // TODO: IMG NOT WORKING
-class CartItem extends Component {
+class WishItem extends Component {
   render() {
     return (
-      <div className="cart-item">
+      <div className="wish-item">
         <img src="../components/blackhole.jpg" alt="" />
         <h1>Item: {this.props.name}</h1>
 
-        {this.props.isNeeded ? <button type="button">Share</button> : <button type="button" disabled>Share</button> }
+        {this.props.isNeeded ? <button type="button">Request</button> : <button type="button" disabled>Request</button> }
         <button type="button" onClick={alert('Asd')}>Trash</button>
       </div>
     );
   }
 }
-export default CartItem;
+export default WishItem;
